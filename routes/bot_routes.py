979c2a3628a1c6e86319d2505423d03cf3726948
flassboot <stefan_florin100@yahoo.com>
2025-02-25 18:48:58 +0100

    Update bot_routes.py

--- routes/bot_routes.py
+++ routes/bot_routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
-from bot.bot import run_bot
+from bot.bot import run_bot  # Asigură-te că există bot/bot.py cu funcția run_bot
 
-bot_routes = Blueprint('bot_routes', __name__)
+bot_blueprint = Blueprint("bot_routes", __name__)
 
-@bot_routes.route("/start-bot", methods=["GET"])
+@bot_blueprint.route("/start-bot", methods=["GET"])
 def start_bot():
     """
     Pornire bot prin API Flask.
