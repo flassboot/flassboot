70800f4d8d71f0ed9957ab9b47b97bc157c09dbc
flassboot <stefan_florin100@yahoo.com>
2025-02-25 18:08:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
+from routes.bot_routes import bot_routes
 
+app = Flask(__name__)
+
+# Adăugare blueprint pentru bot
+app.register_blueprint(bot_routes, url_prefix="/bot")
+
+if __name__ == "__main__":
+    app.run(debug=True)
