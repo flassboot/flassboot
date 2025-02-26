c338e55028e1930705c329b126e50780441c857b
flassboot <stefan_florin100@yahoo.com>
2025-02-26 12:21:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
-from routes.bot_routes import bot_blueprint
+from routes.bot_routes import bot_blue
+from routes.web import web
 
 app = Flask(__name__)
 
-# Importă rutele pentru bot
-app.register_blueprint(bot_blueprint)
+# Înregistrează blueprint-urile pentru bot și rutele web
+app.register_blueprint(bot_blue)
+app.register_blueprint(web)
 
 # Pagina principală
 @app.route('/')
@@ -18,12 +20,3 @@ def about():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-from flask import Flask
-from routes.web import web
-
-app = Flask(__name__)
-
-app.register_blueprint(web)
-
-if __name__ == '__main__':
-    app.run(debug=True)
