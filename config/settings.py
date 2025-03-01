04d39810a3b5b738840c3f99854fcd08596d1d77
flassboot <stefan_florin100@yahoo.com>
2025-03-01 22:46:52 +0100

    Delete config directory

--- config/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    """ Configurație pentru aplicație """
-    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
-    DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
